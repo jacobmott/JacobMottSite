bf45c8621d583563f9abc5dd95220e2b1eb720d1
Jacob Mott <jacobmott@gmail.com>
2022-09-23 08:39:38 +0100

    Intial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgcarouselComponent } from './imgcarousel/imgcarousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgcarouselComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/imgcarousel/imgcarousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imgcarousel',
+  templateUrl: './imgcarousel.component.html',
+  styleUrls: ['./imgcarousel.component.scss']
+})
+export class ImgcarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
